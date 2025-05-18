4f0dfdfa592a0def85477528d07c49f72f1b03b8
Razvanell <stanciurazvanell95@gmail.com>
2025-05-19 01:41:59 +0200

    Code reformat

--- src/main/java/razvanell/musicrays/model/track/TrackRepository.java
+++ src/main/java/razvanell/musicrays/model/track/TrackRepository.java
@@ -4,16 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TrackRepository extends JpaRepository<Track, Long> {
 
-    List<Track> findTop5ByOrderByArtistAsc();
-
     long count();
 
+    @Deprecated
     @Query("SELECT track FROM Track track WHERE track.title = ?1")
     Optional<Track> findByTitle(String title);
 
