214f1aeb302d6d04a05da20fd1ebb2d9f13d66d8
Razvanell <stanciurazvanell95@gmail.com>
2025-05-20 23:33:22 +0200

    Fixed Spring Security. Renamed Registration files to Register. Added AI service to look up info about songs

--- src/main/java/razvanell/musicrays/security/CorsConfig.java
+++ src/main/java/razvanell/musicrays/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:4200",             // Local dev
-                "https://010c-2a02-8388-e539-9700-c941-95d6-e8f8-8b0f.ngrok-free.app"      // Replace with your actual ngrok URL
+                ""      // Replace with your actual ngrok URL
         ));
 
         config.setAllowCredentials(true);
